517021aacccd0fdaa4acba08f34797b20f49ccba
CodeArtist970725 <codeartist0725@gmail.com>
2023-10-30 22:47:05 -0500

    - update cargo

--- movie-app/movie-lib-test/src/main.rs
+++ movie-app/movie-lib-test/src/main.rs
@@ -1,7 +1,7 @@
-fn main(){
-    let mut line = String::new();
-    println!("Enter your name :");
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
-    println!("Hello , {}", line);
-    println!("no of bytes read , {}", b1);
- }
+use std::io::Write;
+fn main() {
+   let mut file = std::fs::File::create("data.txt").expect("create failed");
+   file.write_all("Hello World".as_bytes()).expect("write failed");
+   file.write_all("\nTutorialsPoint".as_bytes()).expect("write failed");
+   println!("data written to file" );
+}
